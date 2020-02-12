7cc95a28fa2ecfce6a3293aa2887d5268210c794
abdelqwy-sayed-ahmed <abdelqwy-sayed-ahmed@github.com>
2020-02-12 14:02:28 +0200

    Version 1.0.0alpha

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
-module.exports={
-  mongoURI:'mongodb+srv://abdelqwy:abdelqwy@cluster0-c4ii2.mongodb.net/test?retryWrites=true&w=majority',
-  googleClientID:'519724621401-1e1rlk1r0g44c1e9t4iqos7ne15lq1pd.apps.googleusercontent.com',
-  googleClientSecret:'ABm3KE3miKAVM7hEKD6Pzvwe',
-  facebookID:'119187029391044',
-  facebookSecret:'8d3dee86b30bc843a2c0162ab7d0f78a'
+if(process.env.NODE_ENV==='production'){
+  module.exports=require('./keys_prod');
+
+}else{
+  module.exports=require('./keys_dev');
 }
---
+++ config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports={
+  mongoURI:process.env.MONGO_URI,
+  googleClientID:process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+  facebookID:process.env.FACEBOOK_ID,
+  facebookSecret:process.env.FACEBOOK_SECRET
+}
